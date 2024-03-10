8a28a18bdcfaf77f36407726044f274a08930db7
Jzhaoqc <zhaoqianchi20021008@gmail.com>
2024-03-09 21:26:18 -0500

    server testing

--- server.c
+++ server.c
@@ -193,9 +193,9 @@ void client_routine(void* arg){
                             //Construct response message
                             if(user_exists){
                                 reply_message.type = TYPE_LO_ACK;
-                                reply_message.size = 0;
-                                strcpy(reply_message.data,"0");
-                                strcpy(reply_message.source,"0");
+                                // reply_message.size = 0;
+                                // strcpy(reply_message.data,"0");
+                                // strcpy(reply_message.source,"0");
                             }else{
                                 char* msg = "Invalid user name or password\n";
                                 reply_message.type = TYPE_LO_NACK;
