57c19bdcd60a717139473476dc8e023540959d61
Jzhaoqc <zhaoqianchi20021008@gmail.com>
2024-03-10 20:40:02 -0400

    finished create_chatroom

--- jamesFunction.c
+++ jamesFunction.c
@@ -2,7 +2,8 @@
 
 //create chatroom node, add to big list
 void create_chatroom(char session_id[], User* user){
-    Chatroom *current, *traverse;
+    Chatroom *current = NULL;
+    Chatroom *traverse = NULL;
     
     //Check if it is the first room
     if(room_list_global->first_room == NULL){
@@ -11,21 +12,25 @@ void create_chatroom(char session_id[], User* user){
         //Create new room and add to gobal list
         current = (Chatroom*) malloc(sizeof(Chatroom));
         room_list_global->first_room = current;
-
-        //Initialize fields
-        strcpy(current->room_name, session_id);
-        current->num_in_room = 1;
-        current->first_member = (Member*) malloc(sizeof(Member));
-        current->first_member->user = user;
-        
     }
-    //Travers to the last chatroom node and add new room at the end
+    //Traverse to the last chatroom node and add new room at the end
     else{
-        traverse = room_list_global->first_node;
+        traverse = room_list_global->first_room;
         while(traverse->next != NULL){
             traverse = traverse->next;
         }
+
+        //Create new room and add to gobal list
+        current = (Chatroom*) malloc(sizeof(Chatroom));
+        traverse->next = current;
     }
+
+    //Initialize room fields
+    strcpy(current->room_name, session_id);
+    current->num_in_room = 1;
+    current->first_member = (Member*) malloc(sizeof(Member));
+    current->first_member->user = user;
+    current->first_member->is_owner = true;
 }
 
 //parse username, check from linked list. broadcast: send message to all file descriptors within chatroom members
